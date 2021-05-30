23ce73efe7dc35d8e0eca82f83fad5db634c2be8
juanvillanuevaahumada <juanvillanuevaahumada@gmail.com>
2021-05-29 22:02:36 -0400

    conflicto de rama alternativa resuelto

--- holamundo.py
+++ holamundo.py
@@ -1,6 +1,3 @@
 print("hola mundo")
-<<<<<<< HEAD
 print("modificacion sobre la rama master")
-=======
 print("modificacion en rama alternativa")
->>>>>>> ramaalternativa
--- otromundo.py
+++ otromundo.py
@@ -1,6 +1,2 @@
 print("hola mundo")
-<<<<<<< HEAD
-print("modificacion sobre la rama master")
-=======
 print("modificacion en rama alternativa")
->>>>>>> ramaalternativa
